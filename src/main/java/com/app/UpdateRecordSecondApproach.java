f0f55b5578a5aa4a8f61bdf7e965150cc6063266
Anand <Anand@DESKTOP-LG9M9HT>
2024-04-11 20:32:18 +0530

    fdg

--- src/main/java/com/app/UpdateRecordSecondApproach.java
+++ src/main/java/com/app/UpdateRecordSecondApproach.java
@@ -9,34 +9,33 @@ import com.app.modal.UserDetails;
 
 	public class UpdateRecordSecondApproach {
 
-	    public static void main(String[] args) {
-	        
-	        Configuration cfg = new Configuration();
-	        
-	        cfg.configure("/com/app/hibernate.cfg.xml");
-	        
-	        SessionFactory sessionFactory = cfg.buildSessionFactory();
-	        
-	        Session session = sessionFactory.openSession();
-	        
-	        UserDetails userDetails = new UserDetails();
-	        
-	        userDetails.setId(3);
-	        
-	        userDetails.setUser_Name("Akanksha Singh kush");
-	        
-	        Transaction tx = session.beginTransaction();
-	        
-	        session.update(userDetails);
-	        
-	        tx.commit();
-	        
-	        System.out.println("Object update successfully...");
-	        
-	        session.close();
-	        sessionFactory.close();
+		   public static void main(String[] args) {
+		        
+		        Configuration cfg = new Configuration();
+		        
+		        cfg.configure("com/rsg/hibernate.cfg.xml");
+		        
+		        SessionFactory sessionFactory = cfg.buildSessionFactory();
+		        
+		        Session session = sessionFactory.openSession();
+		        
+		        UserDetails userDetails = new UserDetails();
+		        
+		        userDetails.setId(1);
+		        
+		        userDetails.setUser_Name("Akansha Singh");
+		        
+		        Transaction tx = session.beginTransaction();
+		        
+		        session.update(userDetails);
+		        
+		        tx.commit();
+		        
+		        System.out.println("Object update successfully...");
+		        
+		        session.close();
+		        sessionFactory.close();
 
-	    }
+		    }
 
-	}
-
+		}
